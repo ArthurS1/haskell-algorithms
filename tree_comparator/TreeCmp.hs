0bfcf2970826b942ef35081cf825a462a4e04cd6
Arthur Soulié <arthur.soulie@tuta.io>
2021-09-19 21:50:38 +0200

    Add ground work for comparing binary trees

--- tree_comparator/TreeCmp.hs
+++ tree_comparator/TreeCmp.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Text.Read
+
+data Tree a = Node a | Edge (Tree a, Tree a) 
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = (parse <$> getLine) >>= createTree >> (putStrLn "Hello World")
+
+createTree :: [Maybe Int] -> IO (Maybe (Tree Int))
+createTree l = pure (Just $ Node 1) :: IO (Maybe (Tree Int))
+
+parse :: String -> [Maybe Int]
+parse s = readMaybe <$> (words s) :: [Maybe Int]
