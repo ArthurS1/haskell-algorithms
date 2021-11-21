505d20d6aa25a17eb966a1baa6f3527a3eff87b8
Arthur Soulié <arthur.soulie@tuta.io>
2021-11-21 21:50:25 +0100

    Switch to stack

--- build.sh
+++
@@ -1,4 +0,0 @@
-# A simple script to build all algorithms
-
-echo "This script will build all algorithms:"
-find . -path *.hs -type f -exec ghc -no-keep-hi-files -no-keep-o-files '{}' \;
--- clean.sh
+++
@@ -1,4 +0,0 @@
-# A simple script to clean all algorithms executables
-
-echo "This script will clean the following executables:"
-find . -maxdepth 2 -mindepth 2 -type f -executable -print -delete
